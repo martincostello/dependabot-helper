de280ee84324837950839b9c92753814ea7844d7
martincostello <martin@martincostello.com>
2022-03-10 11:00:16 +0000

    Add skeleton solution
    Add skeleton solution for the application and tests.

---
+++ src/DependabotHelper/Program.cs
@@ -0,0 +1,10 @@
+// Copyright (c) Martin Costello, 2022. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
---
+++ tests/DependabotHelper.Tests/UnitTests.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.DependabotHelper;
+
+public class UnitTests
+{
+    [Fact]
+    public void Test_That_Always_Passes()
+    {
+        // No-op
+    }
+}
