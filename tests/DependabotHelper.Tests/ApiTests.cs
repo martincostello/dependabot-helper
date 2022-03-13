b99a38cd71b856670923f015e0accd235c676119
martincostello <martin@martincostello.com>
2022-03-13 19:27:23 +0000

    Add API error tests
    Add tests for error responses from the API.

--- tests/DependabotHelper.Tests/ApiTests.cs
+++ tests/DependabotHelper.Tests/ApiTests.cs
@@ -21,6 +21,9 @@ public sealed class ApiTests : IDisposable
         OutputHelper = outputHelper;
         Fixture.SetOutputHelper(OutputHelper);
         _scope = Fixture.Interceptor.BeginScope();
+
+        // TODO Fix scope disposal removing the existing bundle
+        Fixture.Interceptor.RegisterBundle(Path.Combine("Bundles", "oauth-http-bundle.json"));
     }
 
     private AppFixture Fixture { get; }
@@ -57,6 +60,141 @@ public sealed class ApiTests : IDisposable
         problem.Title.ShouldBe("Not Found");
         problem.Detail.ShouldBeNull();
         problem.Type.ShouldBe("https://tools.ietf.org/html/rfc7231#section-6.5.4");
+        problem.Instance.ShouldBeNull();
+    }
+
+    [Fact]
+    public async Task Api_Returns_Http_401_If_Token_Invalid()
+    {
+        // Arrange
+        var builder = new HttpRequestInterceptionBuilder()
+            .Requests()
+            .ForGet()
+            .ForUrl("https://api.github.com/repos/martincostello/website")
+            .ForRequestHeader("Authorization", "Token gho_secret-access-token")
+            .Responds()
+            .WithStatus(StatusCodes.Status401Unauthorized)
+            .WithSystemTextJsonContent(new { });
+
+        builder.RegisterWith(Fixture.Interceptor);
+
+        var client = await CreateAuthenticatedClientAsync();
+
+        // Act
+        var response = await client.GetAsync("/github/repos/martincostello/website/pulls");
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+
+        var problem = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+
+        problem.ShouldNotBeNull();
+        problem.Status.ShouldBe(StatusCodes.Status401Unauthorized);
+        problem.Title.ShouldBe("Unauthorized");
+        problem.Detail.ShouldBeNull();
+        problem.Type.ShouldBe("https://tools.ietf.org/html/rfc7235#section-3.1");
+        problem.Instance.ShouldBeNull();
+    }
+
+    [Fact]
+    public async Task Api_Returns_Http_403_If_Token_Forbidden()
+    {
+        // Arrange
+        var builder = new HttpRequestInterceptionBuilder()
+            .Requests()
+            .ForGet()
+            .ForUrl("https://api.github.com/repos/martincostello/website")
+            .ForRequestHeader("Authorization", "Token gho_secret-access-token")
+            .Responds()
+            .WithStatus(StatusCodes.Status403Forbidden)
+            .WithSystemTextJsonContent(new { });
+
+        builder.RegisterWith(Fixture.Interceptor);
+
+        var client = await CreateAuthenticatedClientAsync();
+
+        // Act
+        var response = await client.GetAsync("/github/repos/martincostello/website/pulls");
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.Forbidden);
+
+        var problem = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+
+        problem.ShouldNotBeNull();
+        problem.Status.ShouldBe(StatusCodes.Status403Forbidden);
+        problem.Title.ShouldBe("Forbidden");
+        problem.Detail.ShouldBeNull();
+        problem.Type.ShouldBe("https://tools.ietf.org/html/rfc7231#section-6.5.3");
+        problem.Instance.ShouldBeNull();
+    }
+
+    [Fact]
+    public async Task Api_Returns_Http_429_If_Api_Rate_Limits_Exceeded()
+    {
+        // Arrange
+        var builder = new HttpRequestInterceptionBuilder()
+            .Requests()
+            .ForGet()
+            .ForUrl("https://api.github.com/repos/martincostello/website")
+            .ForRequestHeader("Authorization", "Token gho_secret-access-token")
+            .Responds()
+            .WithStatus(StatusCodes.Status403Forbidden)
+            .WithSystemTextJsonContent(new { message = "API rate limit exceeded" })
+            .WithResponseHeader("x-ratelimit-limit", "60")
+            .WithResponseHeader("x-ratelimit-remaining", "0")
+            .WithResponseHeader("x-ratelimit-reset", "1377013266");
+
+        builder.RegisterWith(Fixture.Interceptor);
+
+        var client = await CreateAuthenticatedClientAsync();
+
+        // Act
+        var response = await client.GetAsync("/github/repos/martincostello/website/pulls");
+
+        // Assert
+        response.StatusCode.ShouldBe((HttpStatusCode)StatusCodes.Status429TooManyRequests);
+
+        var problem = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+
+        problem.ShouldNotBeNull();
+        problem.Status.ShouldBe(StatusCodes.Status429TooManyRequests);
+        problem.Title.ShouldBe("Too Many Requests");
+        problem.Detail.ShouldBeNull();
+        problem.Type.ShouldBe("https://tools.ietf.org/html/rfc6585#section-4");
+        problem.Instance.ShouldBeNull();
+    }
+
+    [Fact]
+    public async Task Api_Returns_Http_500_If_An_Error_Occurs()
+    {
+        // Arrange
+        var builder = new HttpRequestInterceptionBuilder()
+            .Requests()
+            .ForGet()
+            .ForUrl("https://api.github.com/repos/martincostello/website")
+            .ForRequestHeader("Authorization", "Token gho_secret-access-token")
+            .Responds()
+            .WithStatus(StatusCodes.Status500InternalServerError)
+            .WithSystemTextJsonContent(new { message = "Whoops" });
+
+        builder.RegisterWith(Fixture.Interceptor);
+
+        var client = await CreateAuthenticatedClientAsync();
+
+        // Act
+        var response = await client.GetAsync("/github/repos/martincostello/website/pulls");
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.InternalServerError);
+
+        var problem = await response.Content.ReadFromJsonAsync<ProblemDetails>();
+
+        problem.ShouldNotBeNull();
+        problem.Status.ShouldBe(StatusCodes.Status500InternalServerError);
+        problem.Title.ShouldBe("An error occurred while processing your request.");
+        problem.Detail.ShouldBeNull();
+        problem.Type.ShouldBe("https://tools.ietf.org/html/rfc7231#section-6.6.1");
         problem.Instance.ShouldBeNull();
     }
 
