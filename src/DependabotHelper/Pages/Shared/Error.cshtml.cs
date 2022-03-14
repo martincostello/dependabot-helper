e2951f731fc8b3d9e592a212a0d7705bd6028ee0
martincostello <martin@martincostello.com>
2022-03-14 17:07:29 +0000

    Improve error page
    Improve the error page for scenarios other than HTTP 500.
    Add tests for static content.

--- src/DependabotHelper/Pages/Shared/Error.cshtml.cs
+++ src/DependabotHelper/Pages/Shared/Error.cshtml.cs
@@ -4,6 +4,7 @@
 #pragma warning disable SA1649
 
 using System.Diagnostics;
+using System.Net;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,15 +12,65 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace MartinCostello.DependabotHelper.Pages;
 
 [AllowAnonymous]
+[IgnoreAntiforgeryToken]
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 public sealed class ErrorModel : PageModel
 {
-    public string RequestId { get; set; } = default!;
+    public int ErrorStatusCode { get; private set; } = StatusCodes.Status500InternalServerError;
+
+    public bool IsClientError { get; private set; }
+
+    public string Message { get; set; } = "Sorry, something went wrong.";
+
+    public string? RequestId { get; private set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-    public void OnGet()
+    public string Title { get; set; } = "Error";
+
+    public string Subtitle { get; set; } = "Error";
+
+    public void OnGet(int? id = null)
     {
+        int httpCode = id ?? StatusCodes.Status500InternalServerError;
+
+        if (!Enum.IsDefined(typeof(HttpStatusCode), (HttpStatusCode)httpCode) ||
+            id < StatusCodes.Status400BadRequest ||
+            id > 599)
+        {
+            httpCode = StatusCodes.Status500InternalServerError;
+        }
+
         RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        Subtitle = $"Error (HTTP {httpCode})";
+
+        switch (httpCode)
+        {
+            case StatusCodes.Status400BadRequest:
+                Title = "Bad request";
+                Subtitle = "Bad request (HTTP 400)";
+                Message = "The request was invalid.";
+                IsClientError = true;
+                break;
+
+            case StatusCodes.Status405MethodNotAllowed:
+                Title = "Method not allowed";
+                Subtitle = "HTTP method not allowed (HTTP 405)";
+                Message = "The specified HTTP method was not allowed.";
+                IsClientError = true;
+                break;
+
+            case StatusCodes.Status404NotFound:
+                Title = "Not found";
+                Subtitle = "Page not found (HTTP 404)";
+                Message = "The page you requested could not be found.";
+                IsClientError = true;
+                break;
+
+            default:
+                break;
+        }
+
+        Response.StatusCode = httpCode;
     }
 }
---
+++ tests/DependabotHelper.Tests/ResourceTests.cs
@@ -0,0 +1,99 @@
+﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Net;
+using System.Net.Mime;
+using System.Text.Json;
+using MartinCostello.DependabotHelper.Infrastructure;
+
+namespace MartinCostello.DependabotHelper;
+
+[Collection(AppCollection.Name)]
+public sealed class ResourceTests : IntegrationTests<AppFixture>
+{
+    public ResourceTests(AppFixture fixture, ITestOutputHelper outputHelper)
+        : base(fixture, outputHelper)
+    {
+    }
+
+    [Theory]
+    [InlineData("/sign-in", MediaTypeNames.Text.Html)]
+    [InlineData("/css/site.css", "text/css")]
+    [InlineData("/static/js/main.js", "application/javascript")]
+    [InlineData("/static/js/main.js.map", MediaTypeNames.Text.Plain)]
+    [InlineData("/favicon.png", "image/png")]
+    [InlineData("/humans.txt", MediaTypeNames.Text.Plain)]
+    [InlineData("/manifest.webmanifest", "application/manifest+json")]
+    [InlineData("/robots.txt", MediaTypeNames.Text.Plain)]
+    public async Task Can_Load_Resource(string requestUri, string contentType)
+    {
+        // Arrange
+        using var client = Fixture.CreateClient();
+
+        // Act
+        using var response = await client.GetAsync(requestUri);
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.OK, $"Failed to get {requestUri}. {await response.Content!.ReadAsStringAsync()}");
+        response.Content.Headers.ContentType?.MediaType.ShouldBe(contentType);
+        response.Content.Headers.ContentLength.ShouldNotBeNull();
+        response.Content.Headers.ContentLength.ShouldNotBe(0);
+    }
+
+    [Theory]
+    [InlineData("/")]
+    [InlineData("/configure")]
+    public async Task Cannot_Load_Resource_Unauthenticated(string requestUri)
+    {
+        // Arrange
+        using var client = Fixture.CreateClient();
+
+        // Act
+        using var response = await client.GetAsync(requestUri);
+
+        // Assert
+        response.StatusCode.ShouldBe(HttpStatusCode.Redirect);
+        response.Headers.Location?.PathAndQuery.ShouldBe($"/sign-in?ReturnUrl={Uri.EscapeDataString(requestUri)}");
+    }
+
+    [Fact]
+    public async Task Manifest_Is_Valid_Json()
+    {
+        // Arrange
+        using var client = Fixture.CreateClient();
+        using var response = await client.GetAsync("/manifest.webmanifest");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+
+        string json = await response.Content!.ReadAsStringAsync();
+        var manifest = JsonDocument.Parse(json);
+
+        manifest.RootElement.GetProperty("name").GetString().ShouldBe("Dependabot Helper");
+    }
+
+    [Theory]
+    [InlineData("/error", HttpStatusCode.InternalServerError)]
+    [InlineData("/error?id=200", HttpStatusCode.InternalServerError)]
+    [InlineData("/error?id=400", HttpStatusCode.BadRequest)]
+    [InlineData("/error?id=403", HttpStatusCode.Forbidden)]
+    [InlineData("/error?id=404", HttpStatusCode.NotFound)]
+    [InlineData("/error?id=405", HttpStatusCode.MethodNotAllowed)]
+    [InlineData("/error?id=408", HttpStatusCode.RequestTimeout)]
+    [InlineData("/error?id=500", HttpStatusCode.InternalServerError)]
+    [InlineData("/error?id=599", HttpStatusCode.InternalServerError)]
+    [InlineData("/error?id=600", HttpStatusCode.InternalServerError)]
+    [InlineData("/foo", HttpStatusCode.NotFound)]
+    public async Task Error_Page_Returns_Correct_Status_Code(string requestUri, HttpStatusCode expected)
+    {
+        // Arrange
+        using var client = Fixture.CreateClient();
+
+        // Act
+        using var response = await client.GetAsync(requestUri);
+
+        // Assert
+        response.StatusCode.ShouldBe(expected);
+        response.Content!.Headers.ContentType?.MediaType.ShouldBe("text/html");
+    }
+}
