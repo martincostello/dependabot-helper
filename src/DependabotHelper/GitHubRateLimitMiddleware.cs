d17b00218862d766a087e1f2d998a6cb92634407
martincostello <martin@martincostello.com>
2022-03-14 09:51:30 +0000

    Get rate limit from IGitHubClient
    Get the headers via IGitHubClient, rather than relying on them being set in the accessor.

--- src/DependabotHelper/GitHubRateLimitMiddleware.cs
+++ src/DependabotHelper/GitHubRateLimitMiddleware.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using Octokit;
+
 namespace MartinCostello.DependabotHelper;
 
 public class GitHubRateLimitMiddleware
@@ -12,17 +14,17 @@ public class GitHubRateLimitMiddleware
         _next = next;
     }
 
-    public async Task InvokeAsync(HttpContext context, GitHubRateLimitsAccessor accessor)
+    public async Task InvokeAsync(HttpContext context, IGitHubClient client)
     {
         context.Response.OnStarting(() =>
         {
-            var limits = accessor.Current;
+            var rateLimit = client.GetLastApiInfo()?.RateLimit;
 
-            if (limits?.Limit is not null)
+            if (rateLimit is { } limits)
             {
-                string limit = limits.Limit.Value.ToString(CultureInfo.InvariantCulture);
-                string remaining = limits.Remaining!.Value.ToString(CultureInfo.InvariantCulture);
-                string reset = limits.Resets!.Value.ToUnixTimeSeconds().ToString(CultureInfo.InvariantCulture);
+                string limit = limits.Limit.ToString(CultureInfo.InvariantCulture);
+                string remaining = limits.Remaining.ToString(CultureInfo.InvariantCulture);
+                string reset = limits.ResetAsUtcEpochSeconds.ToString(CultureInfo.InvariantCulture);
 
                 var headers = context.Response.Headers;
                 headers.Add("X-RateLimit-Limit", limit);
