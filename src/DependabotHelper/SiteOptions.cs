cea6b90ae7f58fee479df51157f94c426e694848
martincostello <martin@martincostello.com>
2022-03-15 10:26:30 +0000

    Make meta tags configurable
    Make the site meta tags configurable, rather than being hard-coded.

---
+++ src/DependabotHelper/SiteOptions.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.DependabotHelper;
+
+public sealed class SiteOptions
+{
+    public string Description { get; set; } = string.Empty;
+
+    public string Domain { get; set; } = string.Empty;
+
+    public string Twitter { get; set; } = string.Empty;
+}
