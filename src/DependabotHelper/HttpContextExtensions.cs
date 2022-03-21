de945dbe43ffbcb6bb7d9a17f9178b47468e996d
martincostello <martin@martincostello.com>
2022-03-21 13:51:54 +0000

    Make Dependabot logo an SVG
    Use an inline SVG for the Dependabot logo instead of the favicon.

---
+++ src/DependabotHelper/HttpContextExtensions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace MartinCostello.DependabotHelper;
+
+public static class HttpContextExtensions
+{
+    private const string CspNonceKey = "x-csp-nonce";
+
+    public static string? GetCspNonce(this HttpContext context)
+    {
+        string? nonce = null;
+
+        if (context.Items.TryGetValue(CspNonceKey, out object? value))
+        {
+            nonce = value as string;
+        }
+
+        return nonce;
+    }
+
+    public static void SetCspNonce(this HttpContext context, string? value)
+    {
+        context.Items[CspNonceKey] = value;
+    }
+}
