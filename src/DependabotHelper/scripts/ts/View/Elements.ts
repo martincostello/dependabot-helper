5a8adb004e1a8d3f06fe594c5429cb88caecc369
martincostello <martin@martincostello.com>
2023-03-03 14:00:49 +0000

    Show the active merge method
    Highlight the active merge method for a repository.

--- src/DependabotHelper/scripts/ts/View/Elements.ts
+++ src/DependabotHelper/scripts/ts/View/Elements.ts
@@ -2,14 +2,24 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 export class Elements {
+    static readonly active = 'active';
+    static readonly disabled = 'disabled';
     static readonly hidden = 'd-none';
 
+    static activate(element: Element): void {
+        element.classList.add(Elements.active);
+    }
+
+    static deactivate(element: Element): void {
+        element.classList.remove(Elements.active);
+    }
+
     static disable(element: Element): void {
-        element.setAttribute('disabled', '');
+        element.setAttribute(Elements.disabled, '');
     }
 
     static enable(element: Element): void {
-        element.removeAttribute('disabled');
+        element.removeAttribute(Elements.disabled);
     }
 
     static hide(element: Element) {
