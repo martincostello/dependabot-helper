6e05918d475293c1d1528c8c3e35f12f9f7239ca
Martin Costello <martin@martincostello.com>
2024-04-28 22:30:42 +0100

    Add JsonObject to source generation
    Add `JsonObject` to the JSON source generator.

--- src/DependabotHelper/ApplicationJsonSerializerContext.cs
+++ src/DependabotHelper/ApplicationJsonSerializerContext.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 using MartinCostello.DependabotHelper.Models;
 
@@ -9,6 +10,7 @@ namespace MartinCostello.DependabotHelper;
 
 [ExcludeFromCodeCoverage]
 [JsonSerializable(typeof(IList<Repository>))]
+[JsonSerializable(typeof(JsonObject))]
 [JsonSerializable(typeof(MergePullRequestsResponse))]
 [JsonSerializable(typeof(RepositoryPullRequests))]
 [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase, WriteIndented = true)]
