506a705482db30096d6709bc06a1210d66b394aa
martincostello <martin@martincostello.com>
2024-06-08 13:05:37 +0100

    Fix some native AoT warnings
    - Fix native AoT warnings that are resolvable through package upgrades.
    - Bump Newtonsoft.Json transient dependency to the latest version.
    - Humanizer, Octokit and Newtonsoft.Json still produce warnings, so AoT cannot be enabled yet.

---
+++ src/DependabotHelper/ApplicationLambdaSerializer.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Text.Json;
+using Amazon.Lambda.Serialization.SystemTextJson;
+
+namespace MartinCostello.DependabotHelper;
+
+internal sealed class ApplicationLambdaSerializer() : SourceGeneratorLambdaJsonSerializer<ApplicationJsonSerializerContext>()
+{
+    protected override JsonSerializerOptions CreateDefaultJsonSerializationOptions()
+        => new(ApplicationJsonSerializerContext.Default.Options);
+}
