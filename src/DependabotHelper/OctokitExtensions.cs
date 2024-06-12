ce3b3e709cbc6b9bc895172c85e883ccbcecbe62
Martin Costello <martin@martincostello.com>
2024-06-12 11:13:15 +0100

    Suppress IDE0130
    Suppress new IDE0130 warning.

--- src/DependabotHelper/OctokitExtensions.cs
+++ src/DependabotHelper/OctokitExtensions.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Martin Costello, 2022. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+#pragma warning disable IDE0130
 namespace Octokit;
 
 public static class OctokitExtensions
