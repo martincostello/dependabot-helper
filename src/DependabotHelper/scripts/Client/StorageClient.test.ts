bf63deca3de233e52b63ae33a879c610c02eb164
martincostello <martin@martincostello.com>
2024-09-26 10:10:52 +0100

    Add more Jest tests
    - Add Jest tests for `GitHubClient` and `StorageClient`.
    - Remove DOM dependencies from the TypeScript clients.

--- src/DependabotHelper/scripts/Client/StorageClient.test.ts
+++ src/DependabotHelper/scripts/Client/StorageClient.test.ts
@@ -5,7 +5,64 @@ import { describe, expect, test } from '@jest/globals';
 import { StorageClient } from './StorageClient';
 
 describe('StorageClient', () => {
-    test('should be defined', () => {
-        expect(StorageClient).toBeDefined();
+    test('stores user profiles', async () => {
+        global.localStorage = new LocalStorageMock();
+
+        // Arrange
+        const userId = 'my-user';
+        const client = new StorageClient(userId);
+
+        // Act
+        let owners = client.getOwners();
+        expect(owners).toBeDefined();
+        expect(owners.size).toBe(0);
+
+        // Act
+        client.setOwners('my-company', ['second', 'first']);
+
+        // Assert
+        owners = client.getOwners();
+        expect(owners).toBeDefined();
+        expect(owners.size).toBe(1);
+        expect(owners.get('my-company')).toEqual(['first', 'second']);
+
+        // Act
+        client.setOwners(userId, ['my-project']);
+
+        // Assert
+        owners = client.getOwners();
+        expect(owners).toBeDefined();
+        expect(owners.size).toBe(2);
+        expect(owners.get('my-company')).toEqual(['first', 'second']);
+        expect(owners.get(userId)).toEqual(['my-project']);
     });
 });
+
+class LocalStorageMock {
+    readonly length: number;
+    private store: Record<string, string>;
+
+    constructor() {
+        this.store = {};
+    }
+
+    clear() {
+        this.store = {};
+    }
+
+    getItem(key: string) {
+        return this.store[key] || null;
+    }
+
+    key(index: number): string | null {
+        return Object.keys(this.store)[index] || null;
+    }
+
+    setItem(key: string, value: string) {
+        this.store[key] = value;
+    }
+
+    removeItem(key: string) {
+        delete this.store[key];
+    }
+}
--- src/DependabotHelper/scripts/View/App.ts
+++ src/DependabotHelper/scripts/View/App.ts
@@ -9,12 +9,6 @@ import { Page } from './Page';
 import { RateLimitsElement } from './RateLimitsElement';
 
 export class App {
-    private readonly gitHub: GitHubClient;
-
-    constructor() {
-        this.gitHub = new GitHubClient();
-    }
-
     async initialize(): Promise<void> {
         const userElement = document.querySelector('meta[name="x-user-id"]');
         const userId = userElement?.getAttribute('content') ?? '';
@@ -23,18 +17,36 @@ export class App {
             return;
         }
 
-        const storage = new StorageClient();
+        const storage = new StorageClient(userId);
         const rateLimits = new RateLimitsElement();
         const errors = new ErrorsElement();
 
+        let client: GitHubClient = null;
+
         if (Page.ownerList()) {
-            const manage = new Manage(this.gitHub, storage, rateLimits, errors);
+            client = this.createClient();
+            const manage = new Manage(client, storage, rateLimits, errors);
             await manage.initialize();
         }
 
         if (Page.searchModal()) {
-            const config = new Configuration(this.gitHub, storage, rateLimits, errors);
+            if (!client) {
+                client = this.createClient();
+            }
+            const config = new Configuration(client, storage, rateLimits, errors);
             config.initialize();
         }
+    }
+
+    private createClient(): GitHubClient {
+        const antiforgeryHeader = this.getMetaContent('x-antiforgery-header');
+        const antiforgeryToken = this.getMetaContent('x-antiforgery-token');
+
+        return new GitHubClient(antiforgeryHeader, antiforgeryToken);
+    }
+
+    private getMetaContent(name: string): string {
+        const element = document.querySelector(`meta[name="${name}"]`);
+        return element?.getAttribute('content') ?? '';
     }
 }
