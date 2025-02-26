841c319c638ae04c02f58b96f9e8c9603fcd732d
martincostello <martin@martincostello.com>
2025-02-26 19:37:19 +0000

    Set logging resource builder
    Associate the resource builder with the OTel logging collector too.

--- src/DependabotHelper/TelemetryExtensions.cs
+++ src/DependabotHelper/TelemetryExtensions.cs
@@ -21,14 +21,14 @@ public static class TelemetryExtensions
         ["raw.githubusercontent.com"] = "GitHub",
     };
 
+    public static ResourceBuilder ResourceBuilder { get; } = ResourceBuilder.CreateDefault()
+        .AddService(ApplicationTelemetry.ServiceName, serviceVersion: ApplicationTelemetry.ServiceVersion)
+        .AddAzureAppServiceDetector()
+        .AddContainerDetector();
+
     public static void AddTelemetry(this IServiceCollection services, IWebHostEnvironment environment)
     {
         ArgumentNullException.ThrowIfNull(services);
-
-        var resourceBuilder = ResourceBuilder.CreateDefault()
-            .AddService(ApplicationTelemetry.ServiceName, serviceVersion: ApplicationTelemetry.ServiceVersion)
-            .AddAzureAppServiceDetector()
-            .AddContainerDetector();
 
         if (IsAzureMonitorConfigured())
         {
@@ -40,7 +40,7 @@ public static class TelemetryExtensions
             .AddOpenTelemetry()
             .WithMetrics((builder) =>
             {
-                builder.SetResourceBuilder(resourceBuilder)
+                builder.SetResourceBuilder(ResourceBuilder)
                        .AddAspNetCoreInstrumentation()
                        .AddHttpClientInstrumentation()
                        .AddRuntimeInstrumentation();
@@ -60,7 +60,7 @@ public static class TelemetryExtensions
                 // See https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/Azure.Core/samples/Diagnostics.md
                 AppContext.SetSwitch("Azure.Experimental.EnableActivitySource", true);
 
-                builder.SetResourceBuilder(resourceBuilder)
+                builder.SetResourceBuilder(ResourceBuilder)
                        .AddAspNetCoreInstrumentation()
                        .AddHttpClientInstrumentation()
                        .AddSource(ApplicationTelemetry.ServiceName)
